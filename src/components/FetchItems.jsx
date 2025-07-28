e8747593d51dd1b5cef5e4d19ebbee2dfc6a1b9e
chakit07 <chakitsharma7@gmail.com>
2025-07-28 16:58:58 +0530

    first commit

---
+++ src/components/FetchItems.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStatusActions } from "../store/fetchStatusSlice";
+import { itemsActions } from "../store/itemsSlice";
+
+const FetchItems = () => {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fetchStatus.fetchDone) return;
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    dispatch(fetchStatusActions.markFetchingStarted());
+    fetch("http://localhost:8080/items", { signal })
+      .then((res) => res.json())
+      .then(({ items }) => {
+        console.log("Fetched items", items);
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingFinished());
+        dispatch(itemsActions.addInitialItems(items));
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [fetchStatus]);
+
+  return <></>;
+};
+
+export default FetchItems;
